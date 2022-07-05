8911a620f61df8efd77609752af9afafb9b35ef4
knoooooooow <knoooooooow@outlook.com>
2022-07-06 01:45:17 +0800

    添加卡片歌曲功能

--- src/app/main.py
+++ src/app/main.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 from khl.card import CardMessage, Card, Module, Element, Types, Struct
 from khl import Message, Bot, EventTypes, Event
 
+from app.music import Music
+
 # load config from config/config.json, replace `path` to your own config file
 # config template: `./config/config.json.example`
 with open('../config/config.json', 'r', encoding='utf-8') as f:
@@ -18,20 +20,55 @@ bot = Bot(token=config['token'])
 # or `/m 好久不见`
 @bot.command()
 async def m(msg: Message, name):
-    await msg.reply(f'您想点的歌曲为: {name}')
+    params = {
+        "keywords": name
+    }
+    Music().GET_DICT('search', params)
+    await msg.reply('您想点的歌曲为: {name}')
+
+
+@bot.command()
+async def minfo(msg: Message, text):
+    params = {
+        "keywords": text
+    }
+    result = Music().GET_DICT('search', params)
+    top = result.songs[0]
+    name = top.name
+    artists = top.artists
+    album = top.album
+    artists_name = ''
+    album_name = album.name
+    for x in artists:
+        artists_name = artists_name + x.name + ' '
+    await msg.reply(
+        CardMessage(
+            Card(
+                Module.Header(name),
+                Module.Context(artists_name),
+                Module.Divider(),
+                Module.Section(album_name)
+            )
+        )
+    )
+
 
 @bot.command()
 async def countdown(msg: Message):
     cm = CardMessage()
 
-    c1 = Card(Module.Header('Countdown example'), color='#5A3BD7')  # color=(90,59,215) is another available form
+    # color=(90,59,215) is another available form
+    c1 = Card(Module.Header('Countdown example'), color='#5A3BD7')
     c1.append(Module.Divider())
-    c1.append(Module.Countdown(datetime.now() + timedelta(hours=1), mode=Types.CountdownMode.SECOND))
+    c1.append(Module.Countdown(datetime.now() +
+              timedelta(hours=1), mode=Types.CountdownMode.SECOND))
     cm.append(c1)
 
-    c2 = Card(theme=Types.Theme.DANGER)  # priority: color > theme, default: Type.Theme.PRIMARY
+    # priority: color > theme, default: Type.Theme.PRIMARY
+    c2 = Card(theme=Types.Theme.DANGER)
     c2.append(Module.Section('the DAY style countdown'))
-    c2.append(Module.Countdown(datetime.now() + timedelta(hours=1), mode=Types.CountdownMode.DAY))
+    c2.append(Module.Countdown(datetime.now() +
+              timedelta(hours=1), mode=Types.CountdownMode.DAY))
     cm.append(c2)  # A CardMessage can contain up to 5 Cards
 
     await msg.reply(cm)
@@ -65,7 +102,8 @@ async def struct(msg: Message):
 
 @bot.on_event(EventTypes.MESSAGE_BTN_CLICK)
 async def print_btn_value(_: Bot, e: Event):
-    print(f'''{e.body['user_info']['nickname']} took the {e.body['value']} pill''')
+    print(
+        f'''{e.body['user_info']['nickname']} took the {e.body['value']} pill''')
 
 # everything done, go ahead now!
 bot.run()
---
+++ src/app/music.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+with open('../config/config.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
+
+api = config.music_163.proxy
+
+
+class Music():
+
+    def GET_DICT(self,url,params = {}):
+        url = api + url
+        res = requests.get(url=url,params = params)
+        to_UTF8 = str(res.content, "utf-8")
+        to_json = json.loads(to_UTF8)
+        return to_json
+params = {
+    "keywords":"好久不见"
+}
+print(Music().GET_DICT('search',params))
