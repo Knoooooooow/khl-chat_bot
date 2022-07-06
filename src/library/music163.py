5ff6a7b896361323cad9f5420425f23898732d0a
knoooooooow <knoooooooow@outlook.com>
2022-07-07 00:54:10 +0800

    添加登录token

--- src/library/music163.py
+++ src/library/music163.py
@@ -9,13 +9,18 @@ api = config['music_163']['proxy']
 
 class Music163():
 
-    def GET_DICT(self,url,params = {}):
+    cookie = config['music_163']['music_cookie']
+    uid = config['music_163']['uid']
+
+    def GET_DICT(self,url,params = {},headers = {}):
         url = api + url
-        res = requests.get(url=url,params = params)
+        headers['uid'] = self.uid
+        
+        if self.cookie !='':
+            headers['cookie'] = self.cookie
+
+        res = requests.get(url=url,params = params,headers = headers)
         to_UTF8 = str(res.content, "utf-8")
         to_json = json.loads(to_UTF8)
         return to_json
-# params = {
-#     "keywords":"好久不见"
-# }
-# print(Music().GET_DICT('search',params))
+        
